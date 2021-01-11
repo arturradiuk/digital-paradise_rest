3ec8ee85fcc08307bfa20e0a89369ff54a7445b1
arturradiuk <arturradiuk@gmail.com>
2021-01-11 19:15:21 +0200

    jwt token updating has been implemented

--- src/main/java/com/digitalparadise/web/security/LoginService.java
+++ src/main/java/com/digitalparadise/web/security/LoginService.java
@@ -1,23 +1,26 @@
 package com.digitalparadise.web.security;
 
 
-
 import com.digitalparadise.web.utils.JWTGeneratorVerifier;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.security.enterprise.authentication.mechanism.http.HttpMessageContext;
 import javax.security.enterprise.credential.Credential;
 import javax.security.enterprise.credential.Password;
 import javax.security.enterprise.credential.UsernamePasswordCredential;
 import javax.security.enterprise.identitystore.CredentialValidationResult;
 import javax.security.enterprise.identitystore.IdentityStoreHandler;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 import java.util.logging.Logger;
 
 @RequestScoped
@@ -34,12 +37,12 @@ public class LoginService {
     @POST
     @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.TEXT_PLAIN})
-    public Response authenticate(@NotNull LoginData loginData){
+    public Response authenticate(@NotNull LoginData loginData) {
 
         Credential credential = new UsernamePasswordCredential(loginData.getEmail(), new Password(loginData.getPassword()));
         CredentialValidationResult result = identityStoreHandler.validate(credential);
 
-        if(result.getStatus() == CredentialValidationResult.Status.VALID){
+        if (result.getStatus() == CredentialValidationResult.Status.VALID) {
             return Response.accepted()
                     .type("application/jwt")
                     .entity(JWTGeneratorVerifier.generateJWTString(result))
@@ -49,7 +52,22 @@ public class LoginService {
     }
 
 
-    public static class LoginData{
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.TEXT_PLAIN})
+    @Path("/token-update")
+    public Response updateToken(@Context HttpServletRequest httpServletRequest) {
+        String authorizationHeader = httpServletRequest.getHeader(CustomJWTAuthenticationMechanism.AUTHORIZATION_HEADER);
+        String jwtSerializedToken = authorizationHeader.substring((CustomJWTAuthenticationMechanism.BEARER.length())).trim();
+
+        return Response.accepted()
+                .type("application/jwt")
+                .entity(JWTGeneratorVerifier.generateJWTStringBasedOnJWTString(jwtSerializedToken))
+                .build();
+    }
+
+
+    public static class LoginData {
         private String email;
         private String password;
 
--- src/main/java/com/digitalparadise/web/utils/JWTGeneratorVerifier.java
+++ src/main/java/com/digitalparadise/web/utils/JWTGeneratorVerifier.java
@@ -13,10 +13,11 @@ import java.util.Date;
 
 public class JWTGeneratorVerifier {
 
-    private  static final String SECRET = "8YuS04LvRqjpnGnet02bvcdoLIubmcXEt597Gj1rU6bW2MXHvQM90jNnascqF71jsmbp-co91xqE1hie-xKz68BwqAfukX8pGCpXtlzXxrXF_fz46kTcC1HsbvwDzLpxaoAoRKAtEt0onytN4wflPcNvzWjZvAYVcfhb6ydUofU";
-    private static final long JWT_TIMEOUT_MS = 15 * 60 * 1000;
+    private static final String SECRET = "8YuS04LvRqjpnGnet02bvcdoLIubmcXEt597Gj1rU6bW2MXHvQM90jNnascqF71jsmbp-co91xqE1hie-xKz68BwqAfukX8pGCpXtlzXxrXF_fz46kTcC1HsbvwDzLpxaoAoRKAtEt0onytN4wflPcNvzWjZvAYVcfhb6ydUofU";
+//    private static final long JWT_TIMEOUT_MS = 15 * 60 * 1000;
+    private static final long JWT_TIMEOUT_MS = 1 * 60 * 1000;
 
-    public static String generateJWTString(CredentialValidationResult credential){
+    public static String generateJWTString(CredentialValidationResult credential) {
         try {
             JWSSigner signer = new MACSigner(SECRET);
 
@@ -24,10 +25,10 @@ public class JWTGeneratorVerifier {
                     .subject(credential.getCallerPrincipal().getName())
                     .claim("auth", String.join(",", credential.getCallerGroups()))
                     .issuer("Digital paradise store")
-                    .expirationTime(new Date(new Date().getTime()+JWT_TIMEOUT_MS))
+                    .expirationTime(new Date(new Date().getTime() + JWT_TIMEOUT_MS))
                     .build();
 
-            SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256),claimsSet);
+            SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), claimsSet);
 
             signedJWT.sign(signer);
             return signedJWT.serialize();
@@ -38,12 +39,33 @@ public class JWTGeneratorVerifier {
 
     }
 
+    public static String generateJWTStringBasedOnJWTString(String jwtSerializedToken) {
+        SignedJWT oldJwtToken = null;
+        try {
+            JWSSigner signer = new MACSigner(SECRET);
+            oldJwtToken = SignedJWT.parse(jwtSerializedToken);
+            JWTClaimsSet oldJWTClaimsSet = oldJwtToken.getJWTClaimsSet();
+            JWTClaimsSet newJWTClaimsSet = new JWTClaimsSet.Builder()
+                    .subject(oldJWTClaimsSet.getSubject())
+                    .claim("auth", oldJWTClaimsSet.getClaim("auth"))
+                    .issuer(oldJWTClaimsSet.getIssuer())
+                    .expirationTime(new Date(new Date().getTime() + JWT_TIMEOUT_MS))
+                    .build();
+
+            SignedJWT newSignedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), newJWTClaimsSet);
+            newSignedJWT.sign(signer);
+            return newSignedJWT.serialize();
+        } catch (JOSEException | ParseException e) {
+            e.printStackTrace();
+            return "JWT failure";
+        }
+    }
+
     public static boolean validateJWTSignature(String jwtSerializedToken) {
-        try{
+        try {
             JWSObject jwsObject = JWSObject.parse(jwtSerializedToken);
             JWSVerifier verifier = new MACVerifier(SECRET);
             return jwsObject.verify(verifier);
-
         } catch (JOSEException | ParseException e) {
             e.printStackTrace();
             return false;
